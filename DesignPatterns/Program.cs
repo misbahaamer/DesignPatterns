fa753edd51d6231571fc70cf2f7d95aa52482c1d
Misbah Aamer <misbahuddin.aamer@gmail.com>
2020-10-08 15:10:16 -0400

    builder class

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Linq;
+using DesignPatterns.Builder;
 using DesignPatterns.SOLIDPrinciples;
 
 namespace DesignPatterns
@@ -17,37 +18,13 @@ namespace DesignPatterns
     
     public class Program
     {
-        /// <summary>
-        /// in this design a private field relations is exposed as public field for this class to access it. its  violation of DIP
-        /// </summary>
-        /// <param name="relationships"></param>
-        //public Program(Relationships relationships)
-        //{
-        //    var relations = relationships.Relations;
-        //    foreach (var r in relations.Where(x => x.Item1.Name == "John" && x.Item2 == Relationship.Parent))
-        //    {
-        //        Console.WriteLine($"John has a child called {r.Item3.Name}");
-        //    }
-        //}
-        public Program(IRelationshipBrowser browser)
-        {
-            foreach (var p in browser.FindAllChildrenOf("John"))
-            {
-                Console.WriteLine(p.Name);
-            }
-        }
-
+        
         static void Main(string[] args)
         {
-            var parent = new Person {Name = "John"};
-            var child1 = new Person { Name = "chris" };
-            var child2 = new Person { Name = "mary" };
-
-            var relationships = new Relationships();
-            relationships.AddParentAndChild(parent, child1);
-            relationships.AddParentAndChild(parent, child2);
-
-            new Relationships();
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "hello");
+            builder.AddChild("li", "world");
+            Console.WriteLine(builder.ToString());
         }
 
 
