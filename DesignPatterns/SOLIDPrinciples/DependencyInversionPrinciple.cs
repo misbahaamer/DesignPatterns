c1f5674dfcbb9036db65ab078dc51d6462fefdc2
Misbah Aamer <misbahuddin.aamer@gmail.com>
2020-10-08 10:19:54 -0400

    Dependency Inversion Principle

---
+++ DesignPatterns/SOLIDPrinciples/DependencyInversionPrinciple.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPatterns.SOLIDPrinciples
+{
+    class DependencyInversionPrinciple
+    {
+        //high level parts of the system should not level parts of system directly. instead they should depend through abstraction
+    }
+
+    public enum Relationship
+    {
+        Parent,
+        Child,
+        Sibling
+    }
+    public class Person
+    {
+        public string Name;
+    }
+    public class Relationships : IRelationshipBrowser
+    {
+        private List<(Person, Relationship, Person)> relations = new List<(Person, Relationship, Person)>();
+
+        public void AddParentAndChild(Person parent, Person child)
+        {
+            relations.Add((parent, Relationship.Parent, child));
+            relations.Add((child, Relationship.Parent, parent));
+        }
+        //this private filed is forced to be public for main program to work. violation of DIP instead create abstractions like interfaces
+        //public List<(Person, Relationship, Person)> Relations => relations;
+        public IEnumerable<Person> FindAllChildrenOf(string name)
+        {
+            return relations.Where(x => x.Item1.Name == name && x.Item2 == Relationship.Parent)
+                .Select(x => x.Item3);
+        }
+    }
+    public interface IRelationshipBrowser
+    {
+        IEnumerable<Person> FindAllChildrenOf(string name);
+    }
+    
+}
