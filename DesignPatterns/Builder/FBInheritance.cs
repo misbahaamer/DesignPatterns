a04f39d09288639d75822a7c1ddc7abbe9ce35ac
Misbah Aamer <misbahuddin.aamer@gmail.com>
2020-10-08 21:55:56 -0400

    fluent builde rwith recursive generics

---
+++ DesignPatterns/Builder/FBInheritance.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+using System.Text;
+
+namespace DesignPatterns.Builder
+{
+    class FbInheritance
+    {
+        //fluent builder inheritance with recursive generics
+    }
+
+    public class Person
+    {
+        public string Name;
+        public string Position;
+
+        public class Builder : PersonJobBuilder<Builder>
+        {
+
+        }
+        public static Builder New => new Builder();
+        public override string ToString()
+        {
+            return $"{nameof(Name)} : {Name}, {nameof(Position)}: {Position}";
+        }
+    }
+    public abstract class PersonBuilder
+    {
+        protected Person person = new Person();
+
+        public Person Build()
+        {
+            return person;
+        }
+    
+    }
+    public class PersonInfoBuilder<SELF> : PersonBuilder where SELF : PersonInfoBuilder<SELF>
+    {
+        protected Person person= new Person();
+
+        public SELF Called(string name)
+        {
+            person.Name = name;
+            return (SELF)this;
+        }
+
+    }
+    public class PersonJobBuilder<SELF> : PersonInfoBuilder<PersonJobBuilder<SELF>> where SELF : PersonJobBuilder<SELF>
+    {
+        public SELF WorksAsA(string position)
+        {
+            person.Position = position;
+            return (SELF)this;
+        }
+
+    }
+
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -4,27 +4,20 @@ using System.Configuration;
 using System.IO;
 using System.Linq;
 using DesignPatterns.Builder;
-using DesignPatterns.SOLIDPrinciples;
+
 
 namespace DesignPatterns
 {
 
-    /// <summary>
-    /// Single Responsibility Principle:
-    /// Any class should have a single reason to change
-    /// 
-    /// </summary>
-    /// <param name="args"></param>
     
     public class Program
     {
         
         static void Main(string[] args)
         {
-            var builder = new HtmlBuilder("ul");
-            builder.AddChild("li", "hello").AddChild("li", "world");
-            
-            Console.WriteLine(builder.ToString());
+            var me = Person.New.Called("Misbah").WorksAsA("Dev").Build();
+            Console.WriteLine(me);
+
         }
 
 
